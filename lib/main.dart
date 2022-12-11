236690cede867c3e01fc665267842bf80e0fc635
victor <mac@victors-MacBook-Pro.local>
2022-12-11 22:38:15 +0100

    added a christmass card

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.teal,
       ),
       home: const HomePage(),
     );
--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -1,6 +1,6 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 //Extends state with SingleTinker provider state mixin
 class HomePage extends StatefulWidget {
@@ -33,6 +33,79 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Seasons Greetings",
+          style: TextStyle(color: Colors.black),
+        ),
+        backgroundColor: Colors.transparent,
+      ),
+      body: Center(
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const SizedBox(
+            height: 30,
+          ),
+          Transform(
+              alignment: FractionalOffset.center,
+              transform: Matrix4.identity()
+                ..setEntry(2, 1, 0.0015)
+                ..rotateY(pi * _animation.value),
+              child: Card(
+                child: _animation.value <= 0.5
+                    ? Container(
+                        color: Colors.white,
+                        width: 300,
+                        height: 500,
+                        child: Column(
+                          children: [
+                            Image.asset("images/tree.png"),
+                            const Center(
+                              child: Text(
+                                "Merry Christmas",
+                                textAlign: TextAlign.center,
+                                style:
+                                    TextStyle(fontSize: 40, color: Colors.red),
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                    : Container(
+                        width: 300,
+                        height: 300,
+                        color: Colors.white,
+                        child: Center(
+                          child: Transform(
+                            alignment: FractionalOffset.center,
+                            transform: Matrix4.identity()
+                              ..setEntry(2, 1, 0.0015)
+                              ..rotateY(pi * _animation.value),
+                            child: const Text(
+                              "Happy New Year",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(fontSize: 40, color: Colors.red),
+                            ),
+                          ),
+                        )),
+              )),
+          const SizedBox(
+            height: 30,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              if (_status == AnimationStatus.dismissed) {
+                _controller.forward();
+              } else {
+                _controller.reverse();
+              }
+            },
+            child: const Text("Flip Card"),
+          )
+        ],
+      )),
+    );
   }
 }
