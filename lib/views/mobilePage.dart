9d68bb3ba1df57624b2503726cf375a110594539
Victor <dvicxy@gmail.com>
2022-12-20 13:37:40 +0100

    compressed image files

--- lib/views/mobilePage.dart
+++ lib/views/mobilePage.dart
@@ -1,13 +1,14 @@
+// ignore_for_file: file_names
+
 import 'dart:math';
 
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flipper/model/cardmodel.dart';
 import 'package:flutter/material.dart';
+
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:lottie/lottie.dart';
 
-//Extends state with SingleTinker provider state mixin
 class MobilePage extends StatefulWidget {
   const MobilePage({super.key});
 
@@ -16,7 +17,6 @@ class MobilePage extends StatefulWidget {
 }
 
 class _MobilePageState extends State<MobilePage> with TickerProviderStateMixin {
-  //set up controllers
   late AnimationController _controller;
   late Animation _animation;
   final audioPlayer = AudioPlayer();
@@ -76,14 +76,8 @@ class _MobilePageState extends State<MobilePage> with TickerProviderStateMixin {
         "images/c16.jpeg"),
   ];
   var messages = [
-    "This holiday season, let’s make it a point to cherish what’s truly important in our lives: cookies.",
-    "I told Santa you were good this year and sent him a link to your Pinterest board. Merry Christmas to you!",
-    "This Christmas, may your family be functional and all your batteries be included.",
     "Merry Christmas! I put so much thought into your gift that now it's too late to get it.",
-    "Please note: Christmas is canceled. Apparently, you told Santa you have been good this year … he died laughing.",
-    "Is it just me, or does Santa look younger every year?",
     "Eat. Drink. Be Merry. Have a wonderful Christmas!",
-    "Christmas is the only time of year in which one can sit in front of a dead tree and eat candy out of socks. Enjoy!",
     "May the Christmas Season bring only happiness and joy to you and your family.",
     "The gift of love. The gift of peace. The gift of happiness. May all these be yours at Christmas.",
     "Wishing you a season full of light and laughter for you and your family.",
@@ -92,26 +86,48 @@ class _MobilePageState extends State<MobilePage> with TickerProviderStateMixin {
     "Our family wishes you love, joy and peace … today, tomorrow and always.",
     "I hope the magic of Christmas fills every corner of your heart and home with joy — now and always.",
     "May your holidays sparkle with joy and laughter.",
+    "Wishing you a Christmas that's merry and bright!",
+    "I hope you have a safe and relaxing holiday season.",
+    "I hope your holiday season is full of peace, joy and happiness.",
+    "Merry Christmas with lots of love.",
+    "I hope your Christmas is filled with joy this year!",
+    "Happy Holidays! I hope all of your Christmas wishes come true.",
+    "Jesus is the reason for the season. Merry Christmas!",
+    "May God fill your Yuletide season and all your days with immeasurable prosperity and joy! Merry Christmas!",
+    "May you have the gift of faith, the blessing of hope and the peace of His love at Christmas and always!",
+    "Merry Christmas! I hope you receive one blessing after another this coming year.",
+    "May the Lord grant you and all your loved ones peace, joy and goodwill.",
+    "Wishing you a season that’s merry and bright with the light of God’s love.",
+    "May God’s blessings be yours this Christmas.",
+    "May the true spirit of Christmas shine in your heart and light your path.",
+    "May God bless your life with love and joy this holiday season."
   ];
   var images = [
-    "images/c1.jpeg",
-    "images/c2.jpeg",
-    "images/c3.jpeg",
     "images/c4.jpeg",
-    "images/c5.jpeg",
-    "images/c6.jpeg",
-    "images/c7.jpeg",
-    "images/c8.jpeg",
-    "images/c9.jpeg",
-    "images/c10.jpeg",
+    "images/m1.jpeg",
+    "images/m2.jpeg",
+    "images/m3.jpeg",
     "images/c11.jpeg",
     "images/c12.jpeg",
-    "images/c13.jpeg",
-    "images/c14.jpeg",
-    "images/c15.jpeg",
-    "images/c16.jpeg",
+    "images/d15.jpeg",
+    "images/d17.jpeg",
+    "images/d18.jpeg",
+    "images/d19.jpeg",
+    "images/d20.jpeg",
+    "images/d21.jpeg",
+    "images/d22.jpeg",
+    "images/d14.jpeg",
+    "images/d13.jpeg",
+    "images/d12.jpeg",
+    "images/d11.jpeg",
+    "images/d7.jpeg",
+    "images/d6.jpeg",
+    "images/d10.jpeg",
+    "images/d2.jpeg",
+    "images/d1.jpeg",
+    "images/d9.jpeg",
     "images/tree2.png",
-    'images/tree.png',
+    "images/tree.png",
   ];
 
   @override
@@ -138,26 +154,32 @@ class _MobilePageState extends State<MobilePage> with TickerProviderStateMixin {
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
-        title: Text(
-          "Seasons Greetings",
-          style: GoogleFonts.poppins(textStyle: TextStyle(color: Colors.black)),
+        toolbarHeight: 100,
+        title: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            "Seasons Greetings",
+            style: GoogleFonts.poppins(
+                textStyle: TextStyle(color: Colors.black, fontSize: 20.sp)),
+          ),
         ),
+        centerTitle: true,
         backgroundColor: Colors.transparent,
       ),
-      body: SingleChildScrollView(
+      body: SafeArea(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Center(
-              child: Container(
-                // color: Colors.teal,
-                child: Lottie.network(
-                    "https://assets4.lottiefiles.com/packages/lf20_Qu68m487iG.json",
-                    width: 300.w,
-                    height: 100.h),
-              ),
-            ),
+            // Center(
+            //   child: Container(
+            //     // color: Colors.teal,
+            //     child: Lottie.network(
+            //         "https://assets4.lottiefiles.com/packages/lf20_Qu68m487iG.json",
+            //         width: 300.w,
+            //         height: 100.h),
+            //   ),
+            // ),
             SizedBox(
               height: 10.h,
             ),
@@ -166,69 +188,95 @@ class _MobilePageState extends State<MobilePage> with TickerProviderStateMixin {
                 transform: Matrix4.identity()
                   ..setEntry(3, 2, 0.0015)
                   ..rotateY(pi * _animation.value),
-                child: Card(
-                  elevation: 5.0,
-                  child: _animation.value <= 0.5
-                      ? Container(
-                          color: Colors.white,
-                          width: MediaQuery.of(context).size.width * 0.6,
-                          height: MediaQuery.of(context).size.height * 0.5,
-                          child: Column(
-                            children: [
-                              Image.asset("images/tree.png"),
-                              Lottie.network(
-                                "https://assets4.lottiefiles.com/packages/lf20_ojYQlpe8pq.json",
-                              ),
-                              Center(
-                                child: Text(
-                                  "Merry Christmas",
-                                  textAlign: TextAlign.center,
-                                  style: GoogleFonts.poppins(
-                                      textStyle: TextStyle(
-                                          fontSize: 20.sp, color: Colors.red)),
-                                ),
-                              ),
-                            ],
-                          ),
-                        )
-                      : Container(
-                          width: MediaQuery.of(context).size.width * 0.6,
-                          height: MediaQuery.of(context).size.height * 0.5,
-                          color: Colors.white,
-                          child: Center(
-                            child: Transform(
-                              alignment: FractionalOffset.center,
-                              transform: Matrix4.identity()
-                                ..setEntry(2, 1, 0.0013)
-                                ..rotateY(pi * _animation.value),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Image.asset(
-                                    randomImages,
-                                  ),
-                                  SizedBox(
-                                    height: 10.h,
-                                  ),
-                                  Padding(
+                child: Center(
+                  child: Card(
+                    elevation: 5.0,
+                    child: _animation.value <= 0.5
+                        ? Container(
+                            color: Colors.white,
+                            width: MediaQuery.of(context).size.width * 0.8,
+                            height: MediaQuery.of(context).size.height * 0.75,
+                            child: Column(
+                              children: [
+                                Flexible(
+                                  flex: 1,
+                                  child: Padding(
                                     padding: const EdgeInsets.all(8.0),
-                                    child: Text(
-                                      randomMessage,
-                                      textAlign: TextAlign.center,
-                                      style: GoogleFonts.poppins(
-                                          textStyle: TextStyle(
-                                              fontSize: MediaQuery.of(context)
-                                                      .size
-                                                      .width *
-                                                  0.03,
-                                              color: Colors.red)),
+                                    child: Image.asset(
+                                      "images/d1.jpeg",
+                                      width: MediaQuery.of(context).size.width *
+                                          0.6,
+                                      height:
+                                          MediaQuery.of(context).size.height *
+                                              0.6,
                                     ),
                                   ),
-                                ],
-                              ),
+                                ),
+                                // Lottie.network(
+                                //   "https://assets4.lottiefiles.com/packages/lf20_ojYQlpe8pq.json",
+                                // ),
+
+                                Center(
+                                  child: Text(
+                                    "Merry Christmas",
+                                    textAlign: TextAlign.center,
+                                    style: GoogleFonts.poppins(
+                                        textStyle: TextStyle(
+                                            fontSize: 20.sp,
+                                            color: Colors.red)),
+                                  ),
+                                ),
+                              ],
                             ),
-                          )),
+                          )
+                        : Container(
+                            width: MediaQuery.of(context).size.width * 0.8,
+                            height: MediaQuery.of(context).size.height * 0.75,
+                            color: Colors.white,
+                            child: Center(
+                              child: Transform(
+                                alignment: FractionalOffset.center,
+                                transform: Matrix4.identity()
+                                  ..setEntry(2, 1, 0.0013)
+                                  ..rotateY(pi * _animation.value),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Flexible(
+                                      flex: 2,
+                                      child: Image.asset(
+                                        randomImages,
+                                        width:
+                                            MediaQuery.of(context).size.width *
+                                                0.65,
+                                        height:
+                                            MediaQuery.of(context).size.height *
+                                                0.65,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 10.h,
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text(
+                                        randomMessage,
+                                        textAlign: TextAlign.center,
+                                        style: GoogleFonts.poppins(
+                                            textStyle: TextStyle(
+                                                fontSize: MediaQuery.of(context)
+                                                        .size
+                                                        .width *
+                                                    0.03,
+                                                color: Colors.red)),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            )),
+                  ),
                 )),
             const SizedBox(
               height: 10,
@@ -238,6 +286,8 @@ class _MobilePageState extends State<MobilePage> with TickerProviderStateMixin {
                 var url =
                     "https://www.chosic.com/wp-content/uploads/2020/06/United_States_Marine_Band_-_We_Wish_You_a_Merry_Christmas.mp3";
                 print('play');
+                print(" the lenght is ${messages.length.toString()}");
+                print(" the lenght is ${images.length.toString()}");
 
                 if (_status == AnimationStatus.dismissed) {
                   _controller.forward();
@@ -258,6 +308,7 @@ class _MobilePageState extends State<MobilePage> with TickerProviderStateMixin {
                   // // randomMessage = _cards[index].text;
 
                   randomMessage = (messages..shuffle()).first;
+
                   randomImages = (images..shuffle()).first;
                 });
               },
@@ -269,4 +320,3 @@ class _MobilePageState extends State<MobilePage> with TickerProviderStateMixin {
     );
   }
 }
-//TODO: build for web; mobile display, replace long messages with short messages and 10 more messages and images
